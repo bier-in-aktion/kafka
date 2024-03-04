ea47af7ab56e1ae47100f0e88d5d500f92f44273
Alex Leidwein <alex.leidwein@bab.gv.at>
2024-03-04 01:13:32 +0100

    feat: add topic init script fix #1

---
+++ create-topics.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Wait for kafka to start
+sleep 20
+
+/opt/kafka/bin/kafka-topics.sh \
+    --create \
+    --bootstrap-server kafka-1:19092 \
+    --replication-factor 3 \
+    --partitions 6 \
+    --topic products \
+    --config min.insync.replicas=2
