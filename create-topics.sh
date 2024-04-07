54771a2250b8e597f31a5a8b9a7c4c9ae4202256
Alex Leidwein <alex.leidwein@bab.gv.at>
2024-04-07 14:15:51 +0200

    feat: added product udpate topic

--- create-topics.sh
+++ create-topics.sh
@@ -11,3 +11,12 @@ sleep 20
     --topic products \
     --config min.insync.replicas=2 \
     --config retention.ms=86400000
+
+/opt/kafka/bin/kafka-topics.sh \
+    --create \
+    --bootstrap-server kafka-1:19092 \
+    --replication-factor 3 \
+    --partitions 6 \
+    --topic product-updates \
+    --config min.insync.replicas=2 \
+    --config retention.ms=86400000
